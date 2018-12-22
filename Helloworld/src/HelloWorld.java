a1e5bf20e10da856b79c4ed2a5635cbf3e37469b
saisandeep2010 <saisandeep.2010@gmail.com>
2018-12-22 20:30:07 +0530

    cpushing my changes from eclipse

---
+++ Helloworld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld{
+	
+	public void hello(){
+		System.out.println("bye");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HelloWorld t = new HelloWorld();
+		t.hello();
+		
+	}
+
+}
